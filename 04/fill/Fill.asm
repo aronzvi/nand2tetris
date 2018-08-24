6a1fad9f7b431fce81b3513fb764b5822a24a1e5
Aron Weiner <aronzvi@gmail.com>
2018-08-24 12:40:20 -0300

    project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,65 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LISTENLOOP)
+@KBD
+D=M
+
+// if kbd == 0 goto CLEARSCREEN
+@CLEARSCREEN
+D;JEQ
+
+// else, blacken screen
+// blacken screen: for each (256 rows * 32) word in screen, set to 1111111111111111 
+//i = 0
+@i
+M=0
+
+(BLAKENSCREENLOOP)
+@i
+D=M
+@8192
+D=A-D        // D = 8192 - i
+@LISTENLOOP
+D;JEQ        // if i == 8192 goto LISTENLOOP
+@i
+D=M
+@SCREEN
+A=D+A       // A = SCREEN + i
+M=-1        // M[SCREEN + i] = 1111111111111111
+
+@i          // i = i + 1
+M=M+1
+
+@BLAKENSCREENLOOP // goto BLAKENSCREENLOOP
+0;JMP
+
+
+(CLEARSCREEN)
+//i = 0
+@i
+M=0
+
+(CLEARSCREENLOOP)
+// clearscreen: for each (256 rows * 32 = 8192) word in screen, set to 0000000000000000
+
+@i
+D=M
+@8192
+D=A-D        // D = i - 8192
+@LISTENLOOP
+D;JEQ        // if i == 8192 goto LISTENLOOP
+@i
+D=M
+@SCREEN
+A=D+A       // A = SCREEN + i
+M=0         // M[SCREEN + i] = 0000000000000000
+
+@i          // i = i + 1
+M=M+1
+
+@CLEARSCREENLOOP // goto CLEARSCREENLOOP
+0;JMP
+
+
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,55 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Put your code here
+
+// a = R0
+@R0
+D=M
+
+@a
+M=D
+
+// b = R1
+@R1
+D=M
+
+@b
+M=D
+
+// R2 = 0
+@R2
+M=0
+
+// i = 0
+@i
+M=0
+
+(LOOP)
+// if i == b goto END
+@i
+D=M
+
+@b
+D=M-D
+
+@END
+D;JEQ
+
+// product: R2 = R2 + a 
+@a
+D=M
+
+@R2
+M=D+M
+
+//i++
+@i
+M=M+1
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
